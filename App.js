86e615f55dee8bcafe67b53a1a8ae6704cc52e7d
Joey Yang <yangj30@mcmaster.ca>
2020-09-10 18:02:58 -0400

    Playing around with React Native

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableHighlight,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const handleTextPress = () => {
+    console.log("Text pressed.");
+  };
+
+  const handleImagePress = () => {
+    console.log("Image pressed.");
+  };
+
+  const handleButtonPress = () => {
+    console.log("Button pressed.");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handleTextPress}>2unify</Text>
+      <Image source={require("./assets/icon.png")} />
+      <TouchableHighlight onPress={handleImagePress}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+      <Button title="Button" onPress={handleButtonPress} />
+      <Button
+        title="Alert"
+        onPress={() =>
+          Alert.alert("Alert", "Alert alerted.", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
