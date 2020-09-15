8c0d05493c91e697fa052bcc3c4eec9d7009c5aa
Joey Yang <yangj30@mcmaster.ca>
2020-09-15 17:44:25 -0400

    Added guitar pick button and filler texts.

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,37 +1,58 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View, Button, Image } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  Button,
+  Image,
+  Text,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 const MainScreen = (props) => {
-  const handleButtonPress = () => {
-    console.log("Button pressed.");
+  const handleTuneButtonPress = () => {
+    console.log("Tune button pressed.");
   };
 
   return (
-    <ImageBackground
-      style={styles.background}
-      source={require("../assets/Totoro.jpg")}
-    >
-      <Image source={require("../assets/icon.png")} />
-      <Button
-        title="Tune Button"
-        style={styles.tuneButton}
-        onPress={handleButtonPress}
-      />
-    </ImageBackground>
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.background}
+        source={require("../assets/tunify-static-background.png")}
+      >
+        <Text style={styles.status}>PUSH 2 TUNE.</Text>
+        <TouchableWithoutFeedback onPress={handleTuneButtonPress}>
+          <Image
+            style={styles.tuneButton}
+            source={require("../assets/tunify-guitar-pick-logo.png")}
+          />
+        </TouchableWithoutFeedback>
+        <View style={styles.bottomWidgets}>
+          <Text style={styles.stopTune}>Stop Tune</Text>
+          <Text style={styles.powerOff}>Power Off</Text>
+        </View>
+      </ImageBackground>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   background: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
-  tuneButton: {
-    width: "50%",
-    height: 70,
-    backgroundColor: "#4ecdc4",
+  tuneButton: {},
+  bottomWidgets: {
+    flexDirection: "row",
+    //justifyContent: "space-between",
   },
+  status: {},
+  stopTune: {},
+  powerOff: {},
 });
 
 export default MainScreen;
