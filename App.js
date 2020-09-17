a5da289c0e356367a2c2adf4940ca091742cc336
Joey Yang <yangj30@mcmaster.ca>
2020-09-17 14:05:02 -0400

    Import novaslim google font.

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import MainScreen from "./app/screens/MainScreen";
+import { AppLoading } from "expo";
 
-export default App = () => {
+const App = () => {
   return <MainScreen />;
 };
+
+export default App;
--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -8,32 +8,52 @@ import {
   Text,
   TouchableWithoutFeedback,
 } from "react-native";
+import { AppLoading } from "expo";
+import { useFonts, NovaSlim_400Regular } from "@expo-google-fonts/nova-slim";
 
 const MainScreen = (props) => {
   const handleTuneButtonPress = () => {
     console.log("Tune button pressed.");
   };
 
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.background}
-        source={require("../assets/tunify-static-background.png")}
-      >
-        <Text style={styles.status}>PUSH 2 TUNE.</Text>
-        <TouchableWithoutFeedback onPress={handleTuneButtonPress}>
-          <Image
-            style={styles.tuneButton}
-            source={require("../assets/tunify-guitar-pick-logo.png")}
-          />
-        </TouchableWithoutFeedback>
-        <View style={styles.bottomWidgets}>
-          <Text style={styles.stopTune}>Stop Tune</Text>
-          <Text style={styles.powerOff}>Power Off</Text>
-        </View>
-      </ImageBackground>
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    NovaSlim_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.background}
+          source={require("../assets/tunify-static-background.png")}
+        >
+          <Text style={{ color: "#FFFFFF", fontFamily: "NovaSlim_400Regular" }}>
+            PUSH 2 TUNE.
+          </Text>
+          <TouchableWithoutFeedback onPress={handleTuneButtonPress}>
+            <Image
+              style={styles.tuneButton}
+              source={require("../assets/tunify-guitar-pick-logo.png")}
+            />
+          </TouchableWithoutFeedback>
+          <View style={styles.bottomWidgets}>
+            <Text
+              style={{ color: "#FFFFFF", fontFamily: "NovaSlim_400Regular" }}
+            >
+              Stop Tune
+            </Text>
+            <Text
+              style={{ color: "#FFFFFF", fontFamily: "NovaSlim_400Regular" }}
+            >
+              Power Off
+            </Text>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -50,9 +70,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     //justifyContent: "space-between",
   },
-  status: {},
-  stopTune: {},
-  powerOff: {},
 });
 
 export default MainScreen;
